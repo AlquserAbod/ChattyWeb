89cd74452e6cd605a0b2ccb336cd997b23945939
Alquser Abdullah <96134552+AlquserAbod@users.noreply.github.com>
2024-03-28 17:27:43 +0300

    complete deployment

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -26,7 +26,7 @@ const SearchInput = () => {
   }
   return (
     <form onSubmit={handleSubmit} action="" className="flex items-center gap-2">
-      <input type="text" placeholder="Search.."  className="input input-bordered rounded-full" 
+      <input type="text" placeholder="Search in frinds.."  className="input input-bordered rounded-full" 
       value={search}
       onChange={(e) => setSearch(e.target.value)}/>
       <button className="btn btn-circle bg-sky-500 text-white">
